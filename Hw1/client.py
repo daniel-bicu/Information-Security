1c2d2ef9525383c817184ab20962e9e373c0a9a2
daniel-bicu <danielyoi1233@gmail.com>
2020-11-07 15:09:00 +0200

    Hw1 final added

--- Hw1/client.py
+++ Hw1/client.py
@@ -1,6 +1,5 @@
 import socket
 import sys
-
 import errno
 
 import crypto
@@ -9,17 +8,13 @@ import sys
 sys.modules['Crypto'] = crypto
 import Cryptodome
 from Crypto.Cipher import AES
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from Crypto.Random import get_random_bytes
 
 BLOCK_SIZE = 16  # Bytes
 padd = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * b' '
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 
 def bxor(string_byte1, string_byte2):
-    """ XOR two byte strings """
     return bytes([a1 ^ a2 for a1, a2 in zip(string_byte1, string_byte2)])
 
 
@@ -35,7 +30,7 @@ def client():
     iv = ''
     plaintxt_primit = ''
     first_block_infos = 0
-    last_cripto_block =''
+    last_cripto_block = ''
     # ---------------
 
     HEADER_LENGTH = 1024
@@ -71,9 +66,7 @@ def client():
         try:
 
             while True:
-
                 username_header = client_socket.recv(HEADER_LENGTH)
-
                 if not len(username_header):
                     print('Connection closed by the server')
                     sys.exit()
@@ -142,7 +135,7 @@ def client():
                             client_socket.send(message_header)
                             client_socket.send(message)
                     else:
-                        print(KEY)
+                        #print(KEY)
                         if ECB is True:
                             cipher1 = AES.new(KEY, AES.MODE_ECB)
                             decrypt_block = cipher1.decrypt(message)
@@ -264,7 +257,6 @@ def client():
                         client_socket.send(message_header)
                         client_socket.send(message)
 
-
                         node = "B".encode()
                         node_header = f"{len(node):<{HEADER_LENGTH}}".encode('utf-8')
                         client_socket.send(node_header)
@@ -275,9 +267,9 @@ def client():
                         client_socket.send(message_header)
                         client_socket.send(message)
 
-                        for lg in range(16,len(msg), 16):
+                        for lg in range(16, len(msg), 16):
                             last_cript_txt = crypto_block
-                            text_block = msg[lg:lg+16]
+                            text_block = msg[lg:lg + 16]
                             xored = bxor(last_cript_txt, text_block)
                             crypto_block = cipher1.encrypt(xored)
 
@@ -291,7 +283,7 @@ def client():
                             client_socket.send(message_header)
                             client_socket.send(message)
 
-                    print(KEY)
+                    #print(KEY)
 
                 if my_username == "B" and username == "K":
                     print("sunt B si Am primit key de la K")
@@ -301,12 +293,9 @@ def client():
                     KEY = decrypted_key_from_KM
                     print(KEY)
 
-                print(f' Primeste de la  {username} mesajul: {message}')
-
-
+                print(f'{username} > {message}')
 
         except IOError as e:
-
             if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
                 print('Reading error: {}'.format(str(e)))
                 sys.exit()
--- Hw1/server.py
+++ Hw1/server.py
@@ -1,16 +1,17 @@
 import socket
 import select
 
+# --------------------
+# SERVER INFO NEEDED *
+
 HEADER_LENGTH = 1024
 host = '127.0.0.1'
 PORT = 3300
 
+# ------------------
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# reconnect
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# bind at a port
-
 s.bind((host, PORT))
 s.listen()
 
@@ -33,9 +34,7 @@ def server():
     print("Server started")
     while True:
         read_sockets, _, exception_sockets = select.select(list_of_sockets, [], list_of_sockets)
-        # select.select -: lista citita, lista de scriere, lista de erori
 
-        # mergem pe cei pe care ii vom citii, adica, care au date
         for notified_socket in read_sockets:
             if notified_socket == s:  # daca intalnim serverul, inseamna ca avem o conexiune
                 client_socket, client_address = s.accept()  # socket client , adresa
@@ -53,43 +52,26 @@ def server():
                     f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
 
             else:
-                # daca nu e notificat serverul de o conexiune noua, inseamna ca avem de citit un mesaj
                 node = recv(notified_socket)
                 message = recv(notified_socket)
 
-                # preiau mesajul
-                # daca nu exista...inseamna ca e deconectare
                 if message is False:
                     print(f"Closed Connection from {clients[notified_socket]['data'].decode('utf-8')}")
                     list_of_sockets.remove(notified_socket)
                     del nodes[clients[notified_socket]['data']]
                     del clients[notified_socket]
                     continue
-                # daca totusi exista, ma uit sa vad de la cine e
+
                 user = clients[notified_socket]
                 print(user['data'])
-                # print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
 
-                # dam mai departe informatii / comenzi
                 copies_of_clients = clients.copy()
                 del copies_of_clients[notified_socket]
-                # print(clients[notified_socket])
-                # if user['data'].decode() == 'K':
-                #     nodes['A'.encode()].send(user['header'])
-                #     nodes['A'.encode()].send(user['data'])
-                #     nodes['A'.encode()].send(message['header'])
-                #     nodes['A'.encode()].send(message['data'])
-                #     nodes['B'.encode()].send(user['header'])
-                #     nodes['B'.encode()].send(user['data'])
-                #     nodes['B'.encode()].send(message['header'])
-                #     nodes['B'.encode()].send(message['data'])
-                # else:
+
                 nodes[node['data']].send(user['header'])
                 nodes[node['data']].send(user['data'])
                 nodes[node['data']].send(message['header'])
                 nodes[node['data']].send(message['data'])
-
-            # print(copies_of_clients[client_socket]['data'].decode('utf-8'))
 
 
 if __name__ == '__main__':
